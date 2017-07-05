0b6d473ad696f839e94966bb73d9235d0dc692a7
Zach Schneider <zach@schneid.io>
2017-07-04 23:46:53 -0400

    Refactor and improve code style. * Move functions into plugin class to avoid passing `this`/`self` around * Consistently use `const`/`let` instead of `var`

--- src/index.js
+++ src/index.js
@@ -2,54 +2,6 @@ import path from 'path';
 import url from 'url';
 import RequestShortener from 'webpack/lib/RequestShortener';
 
-function ExtractAssets(modules, requestShortener, publicPath) {
-  var emitted = false;
-  var assets = modules
-    .map(m => {
-      var assets = Object.keys(m.assets || {});
-
-      if (assets.length === 0) {
-        return undefined;
-      }
-
-      var asset = assets[0];
-      emitted = emitted || m.assets[asset].emitted;
-
-      return {
-        name: m.readableIdentifier(requestShortener),
-        asset: asset
-      };
-    }).filter(m => {
-      return m !== undefined;
-    }).reduce((acc, m) => {
-        acc[m.name] = url.resolve(publicPath, m.asset);
-      return acc;
-    }, {});
-
-  return [emitted, assets];
-}
-
-function ExtractChunks(self, chunks, publicPath) {
-  var mappedChunks = chunks
-    .map(c => {
-      return {
-        name: c.name,
-        files: c.files
-          .filter(f => path.extname(f) !== '.map')
-          .map(f => url.resolve(publicPath, f))
-      };
-    })
-    .reduce((acc, c) => {
-      acc[c.name] = c.files;
-      return acc;
-    }, {});
-
-  const emitted = JSON.stringify(self.previousChunks) !== JSON.stringify(mappedChunks);
-  self.previousChunks = mappedChunks;
-
-  return [emitted, mappedChunks];
-}
-
 export default class AssetMapPlugin {
   /**
    * AssetMapPlugin
@@ -63,17 +15,65 @@ export default class AssetMapPlugin {
     this.previousChunks = {};
   }
 
+  _extractAssets(modules, requestShortener, publicPath) {
+    let emitted = false;
+    const mappedAssets = modules
+      .map(m => {
+        const assets = Object.keys(m.assets || {});
+
+        if (assets.length === 0) {
+          return undefined;
+        }
+
+        const asset = assets[0];
+        emitted = emitted || m.assets[asset].emitted;
+
+        return {
+          name: m.readableIdentifier(requestShortener),
+          asset: asset
+        };
+      }).filter(m => {
+        return m !== undefined;
+      }).reduce((acc, m) => {
+          acc[m.name] = url.resolve(publicPath, m.asset);
+        return acc;
+      }, {});
+
+    return [emitted, mappedAssets];
+  }
+
+  _extractChunks(chunks, publicPath) {
+    const mappedChunks = chunks
+      .map(c => {
+        return {
+          name: c.name,
+          files: c.files
+            .filter(f => path.extname(f) !== '.map')
+            .map(f => url.resolve(publicPath, f))
+        };
+      })
+      .reduce((acc, c) => {
+        acc[c.name] = c.files;
+        return acc;
+      }, {});
+
+    const emitted = JSON.stringify(this.previousChunks) !== JSON.stringify(mappedChunks);
+    this.previousChunks = mappedChunks;
+
+    return [emitted, mappedChunks];
+  }
+
   apply(compiler) {
     compiler.plugin('emit', (compilation, done) => {
-      var publicPath = compilation.outputOptions.publicPath;
-      var requestShortener = new RequestShortener(this.relativeTo || path.dirname(this.outputFile));
+      const publicPath = compilation.outputOptions.publicPath;
+      const requestShortener = new RequestShortener(this.relativeTo || path.dirname(this.outputFile));
 
-      var [assetsEmitted, assets] = ExtractAssets(compilation.modules, requestShortener, publicPath);
-      var [chunksEmitted, chunks] = ExtractChunks(this, compilation.chunks, publicPath);
+      const [assetsEmitted, assets] = this._extractAssets(compilation.modules, requestShortener, publicPath);
+      const [chunksEmitted, chunks] = this._extractChunks(compilation.chunks, publicPath);
 
       if (assetsEmitted || chunksEmitted) {
-        var out = JSON.stringify({ assets, chunks }, null, 2);
-        var assetName = this.outputFile.split(compilation.outputOptions.path).pop();
+        const out = JSON.stringify({ assets, chunks }, null, 2);
+        const assetName = this.outputFile.split(compilation.outputOptions.path).pop();
         compilation.assets[assetName] = {
           source: () => {
             return out;
